b4037bad3e9d4024cf368836c8ac271001172404
Pendulousss <yeshkcm@gmail.com>
2025-09-05 07:25:06 +0000

    script skeleton

---
+++ rockpaperscissors.js
@@ -0,0 +1,30 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const userCardsNL = document.querySelectorAll('.usercards>div');
+const userCards = Array.from(userCardsNL);
+
+let compChances = document.querySelector(".complife>p");
+let userChances = document.querySelector(".userlife>p");
+
+let userDisplay = document.querySelector(".userchoice");
+let compDisplay = document.querySelector(".compchoice");
+
+let compChoice = '';
+let userChoice = '';
+
+function getCompChoice() {
+        
+}
+
+function getUserChoice() {
+
+}
+
+function playRound() {
+    
+}
+
+function playGame() {
+
+}
