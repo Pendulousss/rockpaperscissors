a8cc0b3add739d8d841dd57aca334cb5f3db72ce
Pendulousss <yeshkcm@gmail.com>
2025-09-05 09:02:05 +0000

    UI hearts up

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,8 +4,8 @@ const scissors = document.querySelector("#scissors");
 const userCardsNL = document.querySelectorAll('.usercards>div');
 const userCards = Array.from(userCardsNL);
 
-let compChances = document.querySelector(".complife>p");
-let userChances = document.querySelector(".userlife>p");
+let compChances = document.querySelector(".complife .hearts");
+let userChances = document.querySelector(".userlife .hearts");
 
 let userDisplay = document.querySelector(".userchoice");
 let compDisplay = document.querySelector(".compchoice");
@@ -54,34 +54,48 @@ function getUserChoice() {
 
 function playRound() {
     if (userChoice === compChoice) {
-        //alert("It's a tie!");
+        alert("It's a tie!");
     } else if (
         (userChoice === 'rock' && compChoice === 'scissors') ||
         (userChoice === 'paper' && compChoice === 'rock') ||
         (userChoice === 'scissors' && compChoice === 'paper')
     ) {
-        //alert("You win this round!");
-        compChances.textContent = parseInt(compChances.textContent) - 1;
+        alert("You win this round!");
+        let compLives = compChances.querySelectorAll('img').length - 1;
+        updateHearts('comp', compLives);
     } else {
-        //alert("Computer wins this round!");
-        userChances.textContent = parseInt(userChances.textContent) - 1;
+        alert("Computer wins this round!");
+        let userLives = userChances.querySelectorAll('img').length - 1;
+        updateHearts('user', userLives);
     }
     checkGameOver();
 }
 
+function updateHearts(player, lives) {
+    const heartContainer = player === 'comp' ? compChances : userChances;
+    heartContainer.innerHTML = '';
+    
+    for (let i = 0; i < lives; i++) {
+        let heart = document.createElement("img");
+        heart.src = "./images/heart.svg";
+        heart.style.width = "30px";
+        heart.style.height = "30px";
+        heartContainer.appendChild(heart);
+    }
+}
 
 function resetGame() {
-    compChances.textContent = '3';
-    userChances.textContent = '3';
+    updateHearts('comp', 3);
+    updateHearts('user', 3);
     userDisplay.innerHTML = '';
     compDisplay.innerHTML = '';
 }
 
 function checkGameOver() {
-    if (parseInt(compChances.textContent) === 0) {
+    if (compChances.querySelectorAll('img').length === 0) {
         alert("Congratulations! You win the game!");
         resetGame();
-    } else if (parseInt(userChances.textContent) === 0) {
+    } else if (userChances.querySelectorAll('img').length === 0) {
         alert("Sorry, you lose the game!");
         resetGame();
     }
